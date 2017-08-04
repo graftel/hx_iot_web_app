b90336b430a67c6743bbca08f249a572316d3118
Shravya Siluveru <shravya.siluveru8@gmail.com>
2017-08-04 12:16:53 -0500

    Zoom and reset button to zoom

--- src/public/js/drawCharts.js
+++ src/public/js/drawCharts.js
@@ -1,8 +1,9 @@
 	
 	function drawLineGraph(graphDiv, data, assets, domain, YAxis) {
 		graphDiv.select('svg').remove();
-		var vis = graphDiv.append('svg').attr('width', 1000).attr('height', 500)
-				.attr('class', 'col col-md-10'), WIDTH = 800, HEIGHT = 400, MARGINS = {
+		window.vis = graphDiv.append('svg').attr('width', 1000).attr('height', 500)
+				.attr('class', 'col col-md-10');
+		var	WIDTH = 800, HEIGHT = 400, MARGINS = {
 			top : 20,
 			right : 20,
 			bottom : 20,
@@ -19,7 +20,7 @@
 		var xAxis = d3.axisBottom(xScale).ticks(10).tickFormat(function(d) {
 			return timestampToTime(d);
 		});
-		var yAxis = d3.axisLeft(yScale);
+		window.yAxis = d3.axisLeft(yScale);
 	
 		var gX = vis.append("g").attr("class", "xaxis").attr("transform",
 				"translate(0," + (HEIGHT - MARGINS.bottom) + ")");
@@ -32,7 +33,16 @@
 		vis.append("defs").append("clipPath").attr("id", "clip").append("rect")
 				.attr("x", "50").attr("y", "-20").attr("width", WIDTH - 120).attr(
 						"height", HEIGHT);
-	
+		window.zoom = d3.zoom().scaleExtent([ 0.5, 2 ]).on("zoom", function(){
+			var new_xScale = d3.event.transform.rescaleX(xScale);
+	 		var new_yScale = d3.event.transform.rescaleY(yScale);
+	 		gX.call(xAxis.scale(new_xScale)).selectAll("text").attr("y", 0).attr(
+	 				"x", 9).attr("dy", ".35em").attr("transform", "rotate(90)")
+	 			.style("text-anchor", "start");
+	 		gY.call(yAxis.scale(new_yScale));
+	 		vis.selectAll('.line').attr("transform", d3.event.transform);
+		});
+		vis.call(zoom);
 		var chartBody = vis.append("g").attr("clip-path", "url(#clip)");
 		var colors = assignColor(assets);
 		var sideLegend = vis.append('g').attr('class', 'sideLegend');
@@ -105,40 +115,32 @@
 					d3.selectAll(".mouse-per-line circle").style("opacity", "1");
 					d3.selectAll(".mouse-per-line text").style("opacity", "1");
 				})
-				.on(
-						'mousemove',
-						function() { // mouse moving over canvas
-							var mouse = d3.mouse(this);
-							d3.select(".mouse-line").attr("d", function() {
-								var d = "M" + mouse[0] + "," + (HEIGHT - 20);
-								d += " " + mouse[0] + "," + 20;
-								return d;
-							});
+				.on('mousemove', function() { // mouse moving over canvas
+						var mouse = d3.mouse(this);
+						d3.select(".mouse-line").attr("d", function() {
+						var d = "M" + mouse[0] + "," + (HEIGHT - 20);
+							d += " " + mouse[0] + "," + 20;
+							return d;
+						});
 	
-							d3
-									.selectAll(".mouse-per-line")
-									.attr(
-											"transform",
-											function(d, i) {
-												var key = Object.keys(d)[0];
-												if (!($(".sideLegend text[data-id*='"
-														+ key + "']")
-														.hasClass("showLine")))
+						d3.selectAll(".mouse-per-line")
+							.attr("transform", 
+									function(d, i) {
+										var key = Object.keys(d)[0];
+										if (!($(".sideLegend text[data-id*='"+ key + "']").hasClass("showLine")))
 													return null;
-												var xDate = xScale.invert(mouse[0]), bisect = d3
-														.bisector(function(d) {
+										var xDate = xScale.invert(mouse[0]), 
+											bisect = d3.bisector(function(d) {
 															return key;
 														}).right;
-												idx = bisect(d[key], xDate);
+											idx = bisect(d[key], xDate);
 	
-												var beginning = 0, end = lines[i]
+										var beginning = 0, end = lines[i]
 														.getTotalLength(), target = null;
 	
-												while (true) {
-													target = Math
-															.floor((beginning + end) / 2);
-													pos = lines[i]
-															.getPointAtLength(target);
+										while (true) {
+													target = Math.floor((beginning + end) / 2);
+													pos = lines[i].getPointAtLength(target);
 													if ((target === end || target === beginning)
 															&& pos.x !== mouse[0]) {
 														break;
@@ -150,18 +152,10 @@
 													else
 														break; // position found
 												}
-												d3
-														.select(this)
-														.select('text')
-														.text(
-																yScale
-																		.invert(
-																				pos.y)
-																		.toFixed(2));
-	
-												return "translate(" + mouse[0]
-														+ "," + pos.y + ")";
-											});
+										d3.select(this).select('text')
+														.text(yScale.invert(pos.y).toFixed(2));
+										return "translate(" + mouse[0]+ "," + pos.y + ")";
+									});
 						});
 	}
 	
@@ -190,3 +184,11 @@
 		}
 		RadarChart.draw("#sensor-chart", [ data ], config);
 	}
+
+	$( document ).ready(function() {
+		$("#zoom-reset").click(function(event){
+			vis.transition()
+		    .duration(750)
+		    .call(zoom.transform, d3.zoomIdentity);
+		});
+	});
